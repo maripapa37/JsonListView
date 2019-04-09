2f7b4665629b8880ee59d1cbd8e1e9420cadfa61
Maria Papadopoulou <32645900+maripapa37@users.noreply.github.com>
2019-04-09 22:16:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/jsonlistview/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.jsonlistview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        populateUsersList();
+    }
+
+    private void populateUsersList() {
+        // Construct the data source
+        ArrayList<User> arrayOfUsers = User.getUsers();
+        // Create the adapter to convert the array to views
+        CustomUsersAdapter adapter = new CustomUsersAdapter(this, arrayOfUsers);
+        // Attach the adapter to a ListView
+        ListView listView = (ListView) findViewById(R.id.lvUsers);
+        listView.setAdapter(adapter);
+    }
+}
